a583522e8aa9b6c9c781879317d9cb8766d7fb5e
Andrey Meshkov <ay.meshkov@gmail.com>
2016-11-24 15:09:41 +0300

    Update test-cases.js

--- Filters/test-cases.js
+++ Filters/test-cases.js
@@ -1,7 +1,13 @@
 /**
  * Before doing the test, import test_filter.txt to Adguard
  */
+window.addEventListener(function() {
 
-QUnit.test("Test simple element hiding rule", function(assert) {
-    assert.ok(0);
-});
+    QUnit.test("1. Test simple element hiding rule", function(assert) {
+        var element = document.querySelector('#case-1-elemhide > .banner');
+        assert.equal(window.getComputedStyle(element).display, "none");
+    });
+
+    // Add new test cases here
+    
+}, 'load');
