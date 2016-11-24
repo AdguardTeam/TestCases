ac4a1d9db2035474c8aeee3211bfad8c0ad5e206
vozersky <ozersky@gmail.com>
2016-11-24 20:10:42 +0300

    Update test-cases.js

--- Filters/test-cases.js
+++ Filters/test-cases.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function() {
         assert.equal(window.getComputedStyle(element).display, "none");
     });
     
+window.addEventListener('load', function() {    
     QUnit.test("2. Test generic element hiding rule", function(assert) {
         var element = document.querySelector('#case-2-generic-elemhide');
         assert.equal(window.getComputedStyle(element).display, "none");
