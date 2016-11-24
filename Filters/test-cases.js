537ab1f0b4aef7c189effb2b5e68d5215fc305bd
Andrey Meshkov <ay.meshkov@gmail.com>
2016-11-24 15:20:30 +0300

    Update test-cases.js

--- Filters/test-cases.js
+++ Filters/test-cases.js
@@ -3,11 +3,24 @@
  */
 window.addEventListener(function() {
 
-    QUnit.test("1. Test simple element hiding rule", function(assert) {
+    QUnit.test("1. Test domain-specific element hiding rule", function(assert) {
         var element = document.querySelector('#case-1-elemhide > .banner');
         assert.equal(window.getComputedStyle(element).display, "none");
     });
 
     // Add new test cases here
+    // TODO: Generic element-hiding rule
+    // TODO: Domain exclusion: ~adguardteam.github.io##css
+    // TODO: Elemhide exception: adguardteam.github.io#@#css
+    // TODO: Generic CSS rule: #$#css { style }
+    // TODO: Domain-specific CSS rule: adguardteam.github.io#$#css { style }
+    // TODO: Domain exclusion from CSS rule: ~adguardteam.github.io#$#css { style }
+    // TODO: CSS rule exception:adguardteam.github.io#$@#css { style }
+    // TODO: Simple basic rules 
+    // TODO: Basic rules with third-party modifier
+    // TODO: Exception rules (@@)
+    // TODO: Content-type modifiers (script, style, xmlhttprequest, subdocument, media, etc)
+    // TODO: Basic rules with domain restrictions
+    // TODO: JS rules
     
 }, 'load');
