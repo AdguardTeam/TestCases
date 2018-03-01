5ab0de1f5603133b6373df620f75bf37ca445866
mizzick <ds.kolyshev@gmail.com>
2018-03-02 01:00:11 +0700

    Add content rules cases

--- Filters/content-rules/test-content-rules.js
+++ Filters/content-rules/test-content-rules.js
@@ -14,4 +14,12 @@ window.addEventListener('load', function() {
     QUnit.test("3. Test content class", function(assert) {
         assert.notOk(document.querySelector('#case3'));
     });
+
+    QUnit.test("4. Test content wildcard", function(assert) {
+        assert.notOk(document.querySelector('#case4'));
+    });
+
+    QUnit.test("5. Test content exceptions", function(assert) {
+        assert.ok(document.querySelector('#case5'));
+    });
 });
