f01b1b0f73316a04930e76f002ba5af0bab792f6
mizzick <ds.kolyshev@gmail.com>
2018-03-02 00:36:47 +0700

    Add content rules cases

--- Filters/content-rules/test-content-rules.js
+++ Filters/content-rules/test-content-rules.js
@@ -4,17 +4,14 @@
 window.addEventListener('load', function() {
 
     QUnit.test("1. Test content id", function(assert) {
-        var element = document.querySelector('#case1');
-        assert.equal(window.getComputedStyle(element).display, "none");
+        assert.notOk(document.querySelector('#case1'));
     });
 
     QUnit.test("2. Test content content", function(assert) {
-        var element = document.querySelector('#case2');
-        assert.equal(window.getComputedStyle(element).display, "none");
+        assert.notOk(document.querySelector('#case2'));
     });
 
     QUnit.test("3. Test content class", function(assert) {
-        var element = document.querySelector('#case3');
-        assert.equal(window.getComputedStyle(element).display, "none");
+        assert.notOk(document.querySelector('#case3'));
     });
 });
