d4b99edd46b42feb54c6687ef963e06a69463d4f
tvinzz <info@greenstorm.ru>
2019-07-22 20:24:02 +0300

    feature/14

---
+++ Filters/replace-rules/replace-vs-generichide-rule/replace-vs-generichide-rule.js
@@ -0,0 +1,15 @@
+/**
+ * Before doing the test, import test-replace-rules.txt to AdGuard
+ */
+
+window.addEventListener('load', function () {
+
+    QUnit.test("Case 1: using with exception rules including $generichide and $elemhide modifiers for a same request", assert => {
+        const element1 = document.getElementById("case1-block1").textContent;
+        const element2 = document.getElementById("case1-block2").textContent;
+        const expect1 = "$generichide rule doesn't block a $replace rule."
+        const expect2 = "$elemhide rule doesn't block a $replace rule."
+        assert.ok(element1 == expect1 && element2 == expect2, "PASSED! exception rule with $generichide or $elemhide modifiers doesn't disable the $replace rule");
+    });
+
+});
