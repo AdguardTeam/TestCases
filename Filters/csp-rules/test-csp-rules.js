26658fa8a71ae6a02ad0de3b6967e91c6701af48
tvinzz <info@greenstorm.ru>
2019-07-30 16:24:15 +0300

    feature/11

--- Filters/csp-rules/test-csp-rules.js
+++ Filters/csp-rules/test-csp-rules.js
@@ -1,10 +1,17 @@
 /**
  * Before doing the test, import test-csp-rules.txt to AdGuard
  */
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
-    QUnit.test("Case 1: Blocking all remote scripts", function(assert) {
-        assert.equal(document.querySelector("#case1 > h3").innerHTML, "Dummy", "$csp rule should block all remote scripts.");
+    QUnit.test("Case 1: Using with basic rules", function (assert) {
+        assert.notOk(document.getElementById("csp-test") && document.getElementById("some-element"), "$csp rule should work together with basic rules.");
     });
 
+    QUnit.test("Case 2: multiple $csp rules", function (assert) {
+        assert.ok(getComputedStyle(document.querySelector("#google-logo"), null).display === "inline" && getComputedStyle(document.querySelector("#yandex-logo"), null).display === "none", "multiple $csp rules should work toghther.");
+    });
+
+    QUnit.test("Case 3: $scp exception and multiple $csp rules", function (assert) {
+        assert.equal(getComputedStyle(document.querySelector("#case3"), null).display, "none", "$scp exception should disable the $csp rule with matching pattern.");
+    });
 });
