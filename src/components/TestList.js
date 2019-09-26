042bf29cc2a43400e2cd0bbdb979c11899213544
tvinzz <info@greenstorm.ru>
2019-09-26 06:03:51 +0300

    tasks fixes

--- src/components/TestList.js
+++ src/components/TestList.js
@@ -3,38 +3,41 @@ import React from 'react';
 import TestItem from './TestItem';
 import testsData from '../testsData';
 
-const testList = testsData.map(item => (
-    <TestItem key={item.id} {...item} />
-));
-
 export default class TestList extends React.Component {
     state = {}
 
     componentDidMount = () => {
-        this.setState({ list: testList });
+        const fullTestList = testsData.map(item => (
+            <TestItem key={item.id} {...item} />
+        ))
+        this.setState({
+            fullTestList: fullTestList,
+            actualTestList: fullTestList,
+        });
     }
 
-    componentDidUpdate = () => {
-        const message = document.querySelector('.not-found');
-        this.state.list.length ?
-            message.innerHTML = '' :
-            message.innerHTML = 'There is no test matching this name';
-    }
+    escape = string => string.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 
-    searchName = (event) => {
-        const filteredList = [...testList].filter((item) => {
-            const escape = (string) => string.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
-            const keyword = new RegExp(escape(event.target.value, 'i'));
+    searcher = (event) => {
+        const keyword = new RegExp(this.escape(event.target.value).toLowerCase());
+        const { fullTestList } = this.state;
+        const actualTestList = [...fullTestList].filter((item) => {
             const testTitle = item.props.title.toLowerCase();
             if (keyword.test(testTitle)) {
                 return testTitle;
-            } else return null;
+            } else return '';
         })
-        this.setState({ list: filteredList})
+        this.setState({ actualTestList: actualTestList })
     }
 
     render() {
-        const { list } = this.state;
+        const { actualTestList } = this.state;
+        const message = document.querySelector('.not-found');
+        if (message) { 
+            actualTestList.length ?
+                message.innerHTML = '' :
+                message.innerHTML = 'There is no test matching this name.';
+        }
         return (
             <div className="testList-container">
                 <form>
@@ -42,13 +45,13 @@ export default class TestList extends React.Component {
                         type="text"
                         className="search-form"
                         placeholder="Search for the test"
-                        onChange={this.searchName}
+                        onChange={this.searcher}
                         // eslint-disable-next-line jsx-a11y/no-autofocus
                         autoFocus
                     />
                 </form>
                 <span className="not-found" />
-                {list}
+                {actualTestList}
             </div>
         );
     }
