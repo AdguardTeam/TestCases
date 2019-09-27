79df96e2be09196f556b83136a1080cdac7013d6
tvinzz <info@greenstorm.ru>
2019-09-27 17:42:04 +0300

    task fixes

--- public/Filters/redirect-rules/test-redirect-rules.js
+++ public/Filters/redirect-rules/test-redirect-rules.js
@@ -1,9 +1,10 @@
+/* global QUnit */
 /**
  * Before doing the test, import test-redirect-rules.txt to AdGuard
  */
 
 const download = (url) => {
-    const isImage = url.endsWith('.jpg') || url.endsWith('.png');
+    const isImage = url.match(/\.(jpeg|jpg|gif|png)$/) != null;
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.onload = () => {
@@ -33,27 +34,27 @@ window.addEventListener('DOMContentLoaded', function () {
 
     QUnit.test("Case 1: $redirect noopcss test", async assert => {
         const case1 = await download("/test/redirect-test.css");
-        assert.equal(case1, "", "$redirect noopcss rule works");
+        assert.ok(case1 === "", "$redirect noopcss rule works");
     });
 
     QUnit.test("Case 2: $redirect noopjs test", async assert => {
         const case2 = await download("/test/redirect-test.js");
-        assert.equal(case2, "(function() {})()", "$redirect noopjs rule works");
+        assert.ok(case2 === "(function() {})()", "$redirect noopjs rule works");
     });
 
     QUnit.test("Case 3: $redirect images test", async assert => {
         const case3 = await download("/test/redirect-test.jpg");
-        assert.equal(case3, "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAC0lEQVQI12NgQAcAABIAAe+JVKQAAAAASUVORK5CYII=", "$redirect 2x2-transparent.png rule works");
+        assert.ok(case3 === "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAC0lEQVQI12NgQAcAABIAAe+JVKQAAAAASUVORK5CYII=", "$redirect 2x2-transparent.png rule works");
     });
 
     QUnit.test("Case 4: $redirect noopframe test", async assert => {
         const case4 = await download("/test/redirect-test.html");
-        assert.equal(case4.replace(/\n/g, ""), "<!DOCTYPE html><html>    <head><title></title></head>    <body></body></html>", "$redirect noopframe rule works");
+        assert.ok(case4.replace(/\n/g, "") === "<!DOCTYPE html><html>    <head><title></title></head>    <body></body></html>", "$redirect noopframe rule works");
     });
 
     QUnit.test("Case 5: $redirect nooptext test", async assert => {
         const case5 = await download("/test/redirect-test.txt");
-        assert.equal(case5, "", "$redirect nooptext rule works");
+        assert.ok(case5 === "", "$redirect nooptext rule works");
     });
 
     QUnit.test("Case 6: $redirect exception test", async assert => {
@@ -66,7 +67,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     QUnit.test("Case 7: $redirect priority test", async assert => {
         const case7 = await download("/test/redirect-priority-test.js");
-        assert.equal(case7, "(function() {})()", "$redirect rule should have priority over basic rule with $important modifier");
+        assert.ok(case7 === "(function() {})()", "$redirect rule should have priority over basic rule with $important modifier");
     });
 
 });
