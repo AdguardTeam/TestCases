654e951297685413d6f6c70117a5a52d81afec4c
tvinzz <15062885+tvinzz@users.noreply.github.com>
2020-01-09 23:46:54 +0700

    fix  compatibility

--- src/testsData.js
+++ src/testsData.js
@@ -181,8 +181,8 @@ const testsData = [
         title: '$redirect rules',
         link: 'Filters/redirect-rules/test-redirect-rules.html',
         rulesUrl: 'Filters/redirect-rules/test-redirect-rules.txt',
-        compatibility: 'Chrome, FF, Edge, Safari, Opera',
-        incompatibility: 'Windows, MacOS, Android, iOS, Content Blocker',
+        compatibility: 'Chrome, FF, Safari, Opera',
+        incompatibility: 'Edge, Windows, MacOS, Android, iOS, Content Blocker',
         readmeUrl: 'Filters/redirect-rules/README.md',
     },
 ];
