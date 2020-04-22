1b961c26f5f8bfa3954d093f91bd01eee26c243d
tvinzz <o.labudko@adguard.com>
2020-04-22 21:34:26 +0200

    added no-cors option to requests

--- public/Filters/network-rules/test-network-rules.js
+++ public/Filters/network-rules/test-network-rules.js
@@ -4,20 +4,28 @@
  * Before doing the test, import test-network-rules.txt to AdGuard
  */
 
+const download = async (url) => {
+    let response = await fetch(url, {mode: "no-cors"});
+    let responseText = await response.text();
+    return responseText;
+};
+
 window.addEventListener('DOMContentLoaded', function () {
 
     const adgCheck = getComputedStyle(window.document.getElementById('subscribe-to-test-network-rules-filter')).display === 'none';
 
     QUnit.test("Case 1: $network rule test", async assert => {
-        const testImg = document.querySelector("#case1 > img");
-        const isBlocked = (!testImg.style.display) || (getComputedStyle(testImg).width === "0px");
-        assert.ok(adgCheck && isBlocked, "$network rule should block request.");
+        try {
+            await download('https://unit-test4.adguard.com');
+        }
+        catch(error) {
+            assert.ok(true, "$network rule should block request");
+        }
     });
 
     QUnit.test("Case 2: $network exception and priority test", async assert => {
-        const testImg = document.querySelector("#case2 > img");
-        const isBlocked = (!testImg.style.display) || (getComputedStyle(testImg).width === "0px");
-        assert.ok(adgCheck && !isBlocked, "$network exception rule should disable $network rule and reject all other rules.");
+        const result = await download('https://unit-test5.adguard.com');
+        assert.ok(adgCheck && result === 'OK', "$network exception rule should disable $network rule and reject all other rules.");
     });
 
 });
