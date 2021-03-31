920a66d501550dc16a40835ff8a14d0f2093caac
tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
2021-04-01 01:50:17 +0200

    fixed badfilter tests compatibility

--- src/testsData.js
+++ src/testsData.js
@@ -173,8 +173,8 @@ const testsData = [
         title: '$badfilter rules',
         link: 'Filters/badfilter-rules/test-badfilter-rules.html',
         rulesUrl: 'Filters/badfilter-rules/test-badfilter-rules.txt',
-        compatibility: 'Chrome, Edge, FF, Edge Legacy, Safari, Opera',
-        incompatibility: 'Windows, MacOS, Android, iOS, Content Blocker',
+        compatibility: 'Chrome, Edge, FF, Edge Legacy, Safari, Opera, iOS',
+        incompatibility: 'Windows, MacOS, Android, Content Blocker',
         readmeUrl: 'Filters/badfilter-rules/README.md',
     },
     {
