e68544eef95c1d67bf236da235a108a3239b6dda
tvinzz <o.labudko@adguard.com>
2021-07-23 19:19:20 +0300

    added testcase for cookies

--- public/Filters/script-rules/test-script-firefox/test-script-firefox.js
+++ public/Filters/script-rules/test-script-firefox/test-script-firefox.js
@@ -7,6 +7,11 @@ window.addEventListener('load', () => {
     const adgCheck = getComputedStyle(window.document.getElementById('subscribe-to-test-script-firefox-filter'), null).display === 'none';
 
     QUnit.test('1. Test script rule', (assert) => {
-        assert.ok(adgCheck && !window.__firefoxTest1, 'Script rule works');
+        assert.ok(adgCheck && !window.__firefoxTest1, 'Script rule does not work in Firefox');
+    });
+
+    QUnit.test('2. Test script rule for cookie', (assert) => {
+        const cookies = document.cookie;
+        assert.ok(adgCheck && !cookies.includes('adg_test'), 'Script rule for cookies does not work in Firefox');
     });
 });
