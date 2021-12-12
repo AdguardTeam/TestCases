a9b6165923d6a2721f7d56ea410a483724d8c842
tvinzz <o.labudko@adguard.com>
2021-12-13 01:38:56 +0700

    fixed , ,  rules test and updated qunit

--- public/Filters/blocking-request-rules/test-blocking-request-rules.js
+++ public/Filters/blocking-request-rules/test-blocking-request-rules.js
@@ -10,31 +10,31 @@ window.addEventListener('load', () => {
         null
     ).display === 'none';
 
-    // eslint-disable-next-line compat/compat
-    const request = async url => fetch(url, { mode: 'no-cors' });
-
     QUnit.test('1. Test $ping modifier', (assert) => {
-        // eslint-disable-next-line compat/compat
-        assert.ok(adgCheck && navigator.sendBeacon('https://adguard.com', 'Testdata'), 'Rule with $ping modifier blocks navigator.sendBeacon request, check the devtools console');
+        assert.ok(
+            // eslint-disable-next-line compat/compat
+            adgCheck && navigator.sendBeacon('https://adguard.com', 'Testdata'),
+            'Rule with $ping modifier blocks navigator.sendBeacon request, check the devtools console'
+        );
     });
 
     QUnit.test('2. Test $xmlhttprequest modifier', async (assert) => {
-        try {
-            await request('https://adguard-vpn.com');
-            assert.ok(false, 'Rule with $xmlhttprequest modifier should block xmlhttprequest request');
-        } catch (e) {
-            assert.ok(true, 'Rule with $xmlhttprequest modifier blocks xmlhttprequest request');
-        }
+        await assert.rejects(
+            // eslint-disable-next-line compat/compat
+            fetch('https://adguard-vpn.com', { mode: 'no-cors' }),
+            'Rule with $xmlhttprequest modifier should block xmlhttprequest request'
+        );
     });
 
-    QUnit.test('3. Test $websocket modifier', async (assert) => {
+    QUnit.test('3. Test $websocket modifier', (assert) => {
+        const done = assert.async();
         // eslint-disable-next-line compat/compat
         const websocket = new WebSocket('wss://echo.websocket.org');
         websocket.onopen = () => websocket.send('Test message');
-        assert.timeout(3000);
-
         websocket.onerror = () => {
             assert.ok(true, 'Rule with $websocket modifier blocks websocket request');
+            done();
         };
+        assert.timeout(3000);
     });
 });
