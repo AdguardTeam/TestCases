79f6923ae421be5bd8e56deb7e0e60b0a1974b71
Dimitry Kolyshev <dkolyshev@adguard.com>
2022-01-14 14:53:44 +0300

    Pull request #110: Fix/AG-12186 Improve Test
    Merge in ADGTEST/filters-tests from fix/AG-12186 to master
    
    Squashed commit of the following:
    
    commit 90f307a533337180a3ea7dffcab6b51979cd9fb9
    Author: Dimitry Kolyshev <dkolyshev@adguard.com>
    Date:   Wed Jan 12 14:37:44 2022 +0200
    
        upd version
    
    commit 443b35cafbf9b31be9fab1d2f5b565c52b44209b
    Author: Dimitry Kolyshev <dkolyshev@adguard.com>
    Date:   Wed Jan 12 14:33:57 2022 +0200
    
        fix:AG-12186 improve path modifier tests

--- public/Filters/nonbasic-path-modifier/test-nonbasic-path-modifier.js
+++ public/Filters/nonbasic-path-modifier/test-nonbasic-path-modifier.js
@@ -6,31 +6,45 @@
  * Before doing the test, import test-nonbasic-path-modifier.txt to AdGuard
  */
 window.addEventListener('DOMContentLoaded', () => {
+    const adgCheck = getComputedStyle(window.document.getElementById('subscribe-to-test-nonbasic-path-modifier'), null).display === 'none';
+
     QUnit.test('1. Test plain match', (assert) => {
         assert.ok(document.querySelector('#case1'));
         const frame = document.querySelector('#case1 > #subpage1');
         const subDoc = frame.contentDocument || frame.contentWindow.document;
-        assert.notOk(subDoc.querySelector('#case1'));
+
+        const element = subDoc.querySelector('#case1');
+        assert.ok(adgCheck && getComputedStyle(element).display === 'none',
+            'Rule with subpage1 path should be found');
     });
 
     QUnit.test('2. Test plain no match', (assert) => {
         assert.ok(document.querySelector('#case2'));
         const frame = document.querySelector('#case2 > #subpage1');
         const subDoc = frame.contentDocument || frame.contentWindow.document;
-        assert.ok(subDoc.querySelector('#case2'));
+
+        const element = subDoc.querySelector('#case2');
+        assert.ok(adgCheck && getComputedStyle(element).display === 'block',
+            'Rule with subpage2 path should not be found');
     });
 
     QUnit.test('3. Test regex match', (assert) => {
         assert.ok(document.querySelector('#case3'));
         const frame = document.querySelector('#case3 > #subpage1');
         const subDoc = frame.contentDocument || frame.contentWindow.document;
-        assert.notOk(subDoc.querySelector('#case3'));
+
+        const element = subDoc.querySelector('#case3');
+        assert.ok(adgCheck && getComputedStyle(element).display === 'none',
+            'Rule with sub regex path should be found');
     });
 
     QUnit.test('4. Test regex no match', (assert) => {
         assert.ok(document.querySelector('#case4'));
         const frame = document.querySelector('#case4 > #subpage1');
         const subDoc = frame.contentDocument || frame.contentWindow.document;
-        assert.ok(subDoc.querySelector('#case4'));
+
+        const element = subDoc.querySelector('#case4');
+        assert.ok(adgCheck && getComputedStyle(element).display === 'block',
+            'Rule with non-matching regex path should not be found');
     });
 });
