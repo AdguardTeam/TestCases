0bc2b2446ed435892f9b43a8660773cd6ede127a
adguard <devteam@adguard.com>
2022-05-04 08:11:54 +0000

    Deploying to gh-pages from  @ 25330315d4903c962701bbb27141bcc3c025b3cd 🚀

--- Filters/important-rules/test-important-rules.js
+++ Filters/important-rules/test-important-rules.js
@@ -4,6 +4,9 @@
  * Before doing the test, import test-important-rules.txt to AdGuard
  */
 window.addEventListener('DOMContentLoaded', () => {
+    const adgCheck = getComputedStyle(window.document
+        .getElementById('subscribe-to-test-important-rules-filter')).display === 'none';
+
     QUnit.test('Case 1: $important rule vs exception rule', (assert) => {
         const testImg = document.querySelector('#case1 > img');
         const isBlocked = !testImg
@@ -14,6 +17,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     QUnit.test('Case 2: $important rule vs exception $important rule', (assert) => {
         const imageDisplayed = getComputedStyle(document.querySelector('#case2 > img')).display !== 'none';
-        assert.ok(imageDisplayed, 'exception $important rule should have priority over $important rule.');
+        assert.ok(adgCheck && imageDisplayed, 'exception $important rule should have priority over $important rule.');
     });
 });
