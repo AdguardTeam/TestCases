77e424cb1f401eb2e1ad78be923b93c5944a1f66
adguard <devteam@adguard.com>
2023-01-31 13:35:21 +0000

    Deploying to gh-pages from @ AdguardTeam/TestCases@fbd84726b32e8f7b921a3a26488162245a91cd98 🚀

---
+++ Userscripts/TestGrantNone/testscript.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Test Grant None
+// @version      0.1
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('test')
+})();
