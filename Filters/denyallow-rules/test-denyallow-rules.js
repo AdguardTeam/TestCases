2a0ec26c114dab3ba80f5f3091b11ea46f3de0db
adguard <devteam@adguard.com>
2023-02-22 16:57:48 +0000

    Deploying to gh-pages from @ AdguardTeam/TestCases@8aba95f349e4cb91247a2bdc3b470e8897eaa288 🚀

--- Filters/denyallow-rules/test-denyallow-rules.js
+++ Filters/denyallow-rules/test-denyallow-rules.js
@@ -19,41 +19,39 @@ function loadImage(src) {
     }));
 }
 
-// const baseUrl = window.location.origin;
-
 window.addEventListener('DOMContentLoaded', () => {
     QUnit.test('Case 1: $denyallow blocking rule test', async (assert) => {
-        const testLoadImg = await loadImage(
-            'https://cdn.adtidy.org/public/Adguard/Common/adguard_circle.png'
-        );
+        const FIRST_PARTY_URL = '/Filters/denyallow-rules/assets/adguard_circle.png';
+        const THIRD_PARTY_URL = 'https://raw.githubusercontent.com/AdguardTeam/TestCases/master/public/Filters/denyallow-rules/assets/adguard_circle.png';
+
+        const testLoadImg = await loadImage(THIRD_PARTY_URL);
         assert.ok(testLoadImg.height > 5);
-        const testBlockedImg = await loadImage(
-            '/Filters/denyallow-rules/assets/adguard_circle.png'
-        );
+
+        const testBlockedImg = await loadImage(FIRST_PARTY_URL);
         assert.notOk(testBlockedImg.height > 5);
     });
 
     QUnit.test('Case 2: $denyallow whitelist rule test', async (assert) => {
-        const testBlockedImg = await loadImage(
-            'https://cdn.adtidy.org/public/Adguard/Common/adguard_dns_map.png'
-        );
+        const FIRST_PARTY_URL = '/Filters/denyallow-rules/assets/adguard_dns_map.png';
+        const THIRD_PARTY_URL = 'https://raw.githubusercontent.com/AdguardTeam/TestCases/master/public/Filters/denyallow-rules/assets/adguard_dns_map.png';
+
+        const testBlockedImg = await loadImage(THIRD_PARTY_URL);
         assert.notOk(testBlockedImg.height > 5);
-        const testLoadImg = await loadImage(
-            '/Filters/denyallow-rules/assets/adguard_dns_map.png'
-        );
+
+        const testLoadImg = await loadImage(FIRST_PARTY_URL);
         assert.ok(testLoadImg.height > 5);
     });
 
     QUnit.test('Case 3: $denyallow with extra blocking rule', async (assert) => {
-        const testBlockedImg = await loadImage(
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/NCDN_-_CDN.png/800px-NCDN_-_CDN.png'
-        );
+        const BLOCKED_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/NCDN_-_CDN.png/800px-NCDN_-_CDN.png';
+        const testBlockedImg = await loadImage(BLOCKED_URL);
         assert.notOk(testBlockedImg.height > 5);
     });
 
-    QUnit.test('Case 4: $denyallow domain that wasn`t blocked', async (assert) => {
+    QUnit.test('Case 4: $denyallow domain that was not blocked', async (assert) => {
+        const NOT_BLOCKED_URL = 'https://use.fontawesome.com/releases/v5.15.4/css/all.css';
         // eslint-disable-next-line compat/compat
-        const page = await fetch('https://use.fontawesome.com/releases/v5.15.4/css/all.css');
+        const page = await fetch(NOT_BLOCKED_URL);
         assert.ok(page.ok);
     });
 });
