e4a1b8f8b334d73bd5dc04aa85f31b29e7f51fef
Stanislav A <s.atroschenko@adguard.com>
2023-07-12 16:21:06 +0300

    add testscases

--- public/Filters/header-rules/README.md
+++ public/Filters/header-rules/README.md
@@ -6,13 +6,13 @@ Rule with `$header` modifier only blocks request with which it matches by a spec
 Header names are matched case-insensitively.
 Unmatched request does not get blocked.
 ##### Case 1.1
-Rule is being blocked by matching header name only.
+Request is being matched header name only.
 Request which doesn't get header with matching name in response is not blocked.
 ##### Case 1.2
-Rule is being blocked by matching header name and header value.
+Request is being matched by header name and header value.
 Request which doesn't get header with matching name and value in response is not blocked.
 ##### Case 1.3
-Rule is being blocked by matching header name and header value.
+Request is being matched by header name and header value.
 Value is being matched via regexp.
 Request which doesn't get header with matching name and value in response is not blocked.
 
@@ -24,7 +24,11 @@ Unmatched request does not get blocked.
 Basic allowlist rule cancels out blocking rule with `$header` modifier.
 
 ### Case 4
-Allowlist rule unblocks a request that would be blocked by a header rule otherwise, if their `$header` modifier values are the same.
+Allowlist rule with `$header` modifier cancels out blocking rule with `$header` modifier, if they match the same request.
+##### Case 4.1
+Allowlist and blocking rules have same `$header` modifier values, thus request is not blocked.
+##### Case 4.2
+Allowlist $header rule doesn't unblock the request as it's modifier value is not the same as in blocking rule.
 
 ### Case 5
 Blocking rule with `$header` and $important won't be cancelled out by a matching allowlist rule.
@@ -36,13 +40,15 @@ Allowlist `$header` rule that matches request by header value won't cancel out b
 Allowlist `$header` rule that matches request by header value won't cancel out blocking rule with different value of `$header` modifier.
 
 ### Case 8
-Document-level allowlist rule cancels out all blocking `$header` rules.
+Document-level allowlist rule cancels out all blocking `$header` rules. FIXME add this
 
 ### Case 9
 `$header` is compatible with `$removeheader` modifier when targeting response headers.
 ##### Case 9.1
 `$header,$removeheader` rule will remove request's response header if matched by `$header` value.
 ##### Case 9.2
-`$header,$removeheader` rule will be cancelled out by `$removeheader` allowlist rule.
+`$header,$removeheader` rule won't apply if $header doesn't match.
 ##### Case 9.3
+`$header,$removeheader` rule will be cancelled out by `$removeheader` allowlist rule.
+##### Case 9.4
 `$header` allowlist rule won't cancel out `$header,$removeheader` blocking rule, even if `$header` modifier values are the same.
--- public/Filters/header-rules/test-header-rules.js
+++ public/Filters/header-rules/test-header-rules.js
@@ -5,18 +5,123 @@ import { getAgTestRunner } from '../helpers.js';
 const agTest = getAgTestRunner(window.location);
 
 /**
- * Before doing the test, import test-removeheader-rules.txt to AdGuard
+ * Before doing the test, import test-header-rules.txt to AdGuard
  */
 
-const request = async (url) => {
-    // eslint-disable-next-line compat/compat
-    const response = await fetch(url);
-    return response;
-};
-
 // eslint-disable-next-line compat/compat
-const baseUrl = window.location.origin;
+const baseUrl = `${window.location.origin}/Filters/header-rules/test-files`;
+
+const fetchWrapper = async (url) => fetch(`${baseUrl}/${url}`);
 
 window.addEventListener('DOMContentLoaded', () => {
-    agTest(1, '', async (assert) => {});
+    let testFileName;
+    agTest('1.1', 'Case 1.1: Request is being matched header name only.', async (assert) => {
+        testFileName = 'case-1-1.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            'Simple rule blocks by header name only.',
+        );
+    });
+
+    agTest('1.2', 'Case 1.2: Request is being matched by header name and header value.', async (assert) => {
+        testFileName = 'case-1-2.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            'Simple rule blocks by header name and header value.',
+        );
+    });
+
+    agTest('1.3', 'Case 1.3: Request is being matched by header name and header value.', async (assert) => {
+        testFileName = 'case-1-3.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            'Simple rule blocks by header name and header value regexp.',
+        );
+    });
+
+    agTest('2', 'Case 2: Rule wont block the request if header is not matched', async (assert) => {
+        testFileName = 'case-2.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(response.ok, 'Headers not matched, request is ok.');
+    });
+
+    agTest('3', 'Case 3: Request unblocked by basic allowlist rule', async (assert) => {
+        testFileName = 'case-3.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(response.ok, 'Basic allowlist rule unblocks request.');
+    });
+
+    agTest('4.1', 'Case 4.1: Request unblocked by $header allowlist rule', async (assert) => {
+        testFileName = 'case-4-1.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(response.ok, 'Allowlist rule works, request is ok.');
+    });
+
+    agTest('4.2', 'Case 4.2: Request is not unblocked by $header allowlist rule', async (assert) => {
+        testFileName = 'case-4-2.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            '$header values were not matched, request is blocked.',
+        );
+    });
+
+    agTest('5', 'Case 5: important blocking rule blocks through matching allowlist rule', async (assert) => {
+        testFileName = 'case-5.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            'Request is blocked by important $header rule.',
+        );
+    });
+
+    agTest('6', 'Case 6: matching @@$header rule won`t cancel basic blocking rule', async (assert) => {
+        testFileName = 'case-6.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            '@@$header rule matches the request, but don`t cancel basic blocking rule.',
+        );
+    });
+
+    agTest('7', 'Allowlist rule must fully match blocking rule`s $header value to cancel it', async (assert) => {
+        testFileName = 'case-7.json';
+        assert.rejects(
+            fetchWrapper(testFileName),
+            '$header values don`t match, allowlist rule is not applied.',
+        );
+    });
+
+    agTest('9.1', '$header,removeheader works of $header matches', async (assert) => {
+        testFileName = 'case-9-1.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(
+            !response.headers.get('vary'),
+            '$header,removeheader rule removes passed parameter in a response',
+        );
+    });
+
+    agTest('9.2', '$header,removeheader doesn`t apply if $header doesn`t match', async (assert) => {
+        testFileName = 'case-9-2.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(
+            !!response.headers.get('vary'),
+            '$header,removeheader rule is not applied',
+        );
+    });
+
+    agTest('9.3', '$header,removeheader is cancelled by corresponding @@ rule', async (assert) => {
+        testFileName = 'case-9-3.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(
+            !!response.headers.get('vary'),
+            '$header,removeheader rule is not applied',
+        );
+    });
+
+    agTest('9.4', '$header,removeheader is not cancelled by @@$header rule', async (assert) => {
+        testFileName = 'case-9-4.json';
+        const response = await fetchWrapper(testFileName);
+        assert.ok(
+            !response.headers.get('vary'),
+            '$header,removeheader rule is applied',
+        );
+    });
 });
