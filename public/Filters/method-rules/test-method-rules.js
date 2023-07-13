e2b1150fcd5031b69f774f53693d08f36ded5ffc
Stanislav A <s.atroschenko@adguard.com>
2023-07-13 12:26:14 +0300

    lint

--- public/Filters/method-rules/test-method-rules.js
+++ public/Filters/method-rules/test-method-rules.js
@@ -13,7 +13,7 @@ const baseUrl = `${window.location.origin}/Filters/method-rules/test-files`;
 window.addEventListener('DOMContentLoaded', () => {
     let testDataUrl;
     agTest(1, '$method rule blocks requests by specified method', async (assert) => {
-       testDataUrl= `${baseUrl}/test-case-1.json`;
+       testDataUrl = `${baseUrl}/test-case-1.json`;
         assert.rejects(fetch(testDataUrl, {
             cache: 'no-cache',
             method: 'GET',
@@ -27,7 +27,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     agTest(2, '$method rule unblocks requests by specified method', async (assert) => {
-        testDataUrl= `${baseUrl}/test-case-2.json`;
+        testDataUrl = `${baseUrl}/test-case-2.json`;
         assert.rejects(fetch(testDataUrl, {
             cache: 'no-cache',
             method: 'GET',
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     agTest(3, '$method rule with inverted value', async (assert) => {
-        testDataUrl= `${baseUrl}/test-case-3.json`;
+        testDataUrl = `${baseUrl}/test-case-3.json`;
         assert.rejects(fetch(testDataUrl, {
             cache: 'no-cache',
             method: 'GET',
@@ -55,7 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     agTest(4, '$method rule with inverted value', async (assert) => {
-        testDataUrl= `${baseUrl}/test-case-4.json`;
+        testDataUrl = `${baseUrl}/test-case-4.json`;
         assert.rejects(fetch(testDataUrl, {
             cache: 'no-cache',
             method: 'GET',
