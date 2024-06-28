f531ae9cca4f59ea5cf3f094b6647558afa8d651
Dávid Tóta <d.tota@adguard.com>
2024-06-28 20:55:31 +0300

    AG-33870 Redirect resource tests fail
    Merge in ADGTEST/filters-tests from fix/AG-33870 to master
    
    Squashed commit of the following:
    
    commit f8c30c9dffcdafd306de8f431f7d4709b7096695
    Merge: a4af5c9 40bdee9
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Fri Jun 28 19:47:29 2024 +0200
    
        Merge branch 'master' into fix/AG-33870
    
    commit a4af5c981cd04963b62bf861054f43fcabd0daec
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Wed Jun 26 15:54:01 2024 +0200
    
        add note about wsl
    
    commit 6f2c4980c38555c91499f4106f7606dd4b3c7490
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Wed Jun 26 15:48:53 2024 +0200
    
        fix pre push hook on windows
    
    commit 8aa49cd9af001e1031767dcd56e7332de59a9dfa
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Wed Jun 26 15:40:44 2024 +0200
    
        update install instructions
    
    commit 3d5632eb0bb4c781b60ca215f5d67f55189d1a55
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Wed Jun 26 15:40:35 2024 +0200
    
        update redirect security tests

--- README.md
+++ README.md
@@ -17,52 +17,55 @@ Static data is stored in the `build` directory, CF pages functions are in the `f
 
 While making any [tests data](#tests-data) changes, run `yarn build:static` to rebuild public static data needed for autotesting.
 
-### Test on the local machine
+### Test on the local machine (MacOS)
 
-#### Install all dependencies
+1. Install dependencies: `yarn install`
+2. Add to the `/etc/hosts` next line:
+    ```hosts
+    127.0.0.1 local.testcases.agrd.dev
+    ```
+3. Create `cert` directory if there is no one in the repository root: `mkdir cert`
+4. [Install `mkcert`](https://github.com/FiloSottile/mkcert#readme)
+5. Create locally-trusted development certificate:
+   ```shell
+   # Install root certificate
+   mkcert -install
+   # Create certificate for the domain
+   mkcert -key-file cert/key.pem -cert-file cert/cert.pem local.testcases.agrd.dev
+   ```
+6. Build static content: `yarn build`
+7. Run the local server: `yarn watch`
 
-```shell
-yarn install
-```
-
-#### Add to the `/etc/hosts` next line
-
-```shell
-127.0.0.1 local.testcases.agrd.dev
-```
-
-#### Create `cert` directory
-
-if there is no one in the repository root
-
-#### Install [`mkcert`](https://github.com/FiloSottile/mkcert#readme)
-
-```shell
-brew install mkcert
-```
-
-#### Create locally-trusted development certificate
-
-```shell
-mkcert -install
-mkcert -key-file cert/key.pem -cert-file cert/cert.pem local.testcases.agrd.dev
-```
-
-#### Build static content
-
-```shell
-yarn build
-```
-
-#### Run the local server
-
-```shell
-yarn watch
-```
-
-Thr app will be available on `https://local.testcases.agrd.dev:4000/`
+The app will be available on `https://local.testcases.agrd.dev:4000/`
 
 > **Please note, that AdGuard for Mac does not filter localhost connections by default!**.
+
+### Test on the local machine (Windows)
+
+If you are using Windows, you can run the app locally with the following steps:
+
+1. Install dependencies: `yarn install`
+2. Add to the `C:\Windows\System32\drivers\etc\hosts` next line:
+    ```hosts
+    127.0.0.1 local.testcases.agrd.dev
+    ```
+3. Create `cert` directory if there is no one in the repository root: `mkdir cert`
+4. [Install `mkcert`](https://github.com/FiloSottile/mkcert?tab=readme-ov-file#windows) (or you can built it from `mkcert` sources by issuing `go build` command which produces `mkcert.exe` file, but it requires `go` installed)
+5. Create locally-trusted development certificate:
+   ```shell
+   # Install root certificate
+   mkcert -install
+   # Create certificate for the domain
+   mkcert -key-file cert/key.pem -cert-file cert/cert.pem local.testcases.agrd.dev
+   ```
+6. Build static content: `yarn build`
+7. Run the local server: `yarn watch`
+
+> **Note 1:** if you want to uninstall the root certificate, you can use `mkcert -uninstall` command.
+
+> **Note 2:** it is not recommended to run the app on WSL2, because its an OS withing an OS,
+> which means two different hosts files, two certificate store, etc.
+> Its better to use it on Windows directly.
 
 ## <a name="tests-data"></a> Manage tests list
 
--- public/Filters/redirect-security/test-redirect-security.js
+++ public/Filters/redirect-security/test-redirect-security.js
@@ -13,50 +13,62 @@ const agTest = getAgTestRunner(window.location);
 /**
  * Makes requests with used secret key and without secret key and checks the results
  *
- * @param {array} urls
- * @param assert
+ * @param {object} assert QUnit assert object
+ * @param {array} data Array of objects with url and isBlocking properties
  */
-const redirectResourcesSecurityTest = async (assert, urls) => {
+const redirectResourcesSecurityTest = async (assert, data) => {
     /* eslint-disable-next-line no-restricted-syntax */
-    for (const url of urls) {
+    for (const { url, isBlocking } of data) {
         // first request
         // eslint-disable-next-line no-await-in-loop, compat/compat
         const response1 = await fetch(url);
-        assert.ok(
-            response1.status === 200
-            && response1.redirected
-            && response1.url.includes('?secret='),
-            `First request for ${url} is ok`,
-        );
 
-        // second request with the same secret key
-        // eslint-disable-next-line no-await-in-loop
-        await assert.rejects(
-            // eslint-disable-next-line compat/compat
-            fetch(response1.url),
-            'Second request with the same secret key should fail',
-        );
+        if (isBlocking) {
+            // blocking resources should be redirected to a page with a secret key
+            assert.ok(
+                response1.status === 200
+                && response1.redirected
+                && response1.url.includes('?secret='),
+                `First request for ${url} is ok`,
+            );
 
-        // get url without secret key
-        const urlNoSecret = response1.url.substring(0, response1.url.indexOf('?secret='));
+            // second request with the same secret key
+            // eslint-disable-next-line no-await-in-loop
+            await assert.rejects(
+                // eslint-disable-next-line compat/compat
+                fetch(response1.url),
+                'Second request with the same secret key should fail',
+            );
 
-        // third request without secret key
-        // eslint-disable-next-line no-await-in-loop
-        await assert.rejects(
-            // eslint-disable-next-line no-await-in-loop, compat/compat
-            fetch(urlNoSecret),
-            'Third request without secret key should fail',
-        );
+            // get url without secret key
+            const urlNoSecret = response1.url.substring(0, response1.url.indexOf('?secret='));
+
+            // third request without secret key
+            // eslint-disable-next-line no-await-in-loop
+            await assert.rejects(
+                // eslint-disable-next-line no-await-in-loop, compat/compat
+                fetch(urlNoSecret),
+                'Third request without secret key should fail',
+            );
+        } else {
+            // non-blocking redirects don't use native urls anymore, thus making
+            // these tests obsolete for all redirect resources except for click2load,
+            // which is not actually being tested here. AG-19259
+            assert.ok(
+                response1.status === 200
+                && response1.redirected
+                && response1.url.startsWith('data:'),
+                `First request for ${url} is ok`,
+            );
+        }
     }
 };
 
 window.addEventListener('DOMContentLoaded', () => {
     agTest(1, '$redirect resources security test', async (assert) => {
         await redirectResourcesSecurityTest(assert, [
-            '../redirect-rules/test-files/redirect-test.png',
-            '../redirect-rules/test-files/redirect-test.txt',
-            '../redirect-rules/test-files/redirect-test.js',
-            '../redirect-rules/test-files/redirect-test.html',
+            { url: '../redirect-rules/test-files/redirect-test.html', isBlocking: true },
+            { url: '../redirect-rules/test-files/redirect-test.png', isBlocking: false },
         ]);
     });
 });
