0fc1f6b7b8569d4431db4eb3cbdb416f78e5a657
Alexey Zhavoronkov <a.zhavoronkov@adguard.com>
2025-06-19 14:23:56 +0300

    [AG-43083] Specify accept header to make these tests work
    Because $ urltransform rule no longer matches all request types, only document.
    
    See-also: AG-43083
    Signed-off-by: Alexey Zhavoronkov <a.zhavoronkov@adguard.com>

--- public/Filters/urltransform-rules/test-urltransform-rules.js
+++ public/Filters/urltransform-rules/test-urltransform-rules.js
@@ -6,18 +6,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const adgCheck = isSubscribed('subscribe-to-test-urltransform-rules-filter');
 
     agTest(1, '$urltransform rule', async (assert) => {
-        const response = await fetch('https://httpbin.agrd.dev/status/500');
+        const response = await fetch('https://httpbin.agrd.dev/status/500', {
+            headers: { 'Accept': 'text/html' }
+        });
         assert.ok(adgCheck && response.ok, '$urltransform rule should transform an invalid URL into a valid one');
     });
 
     agTest(2, "$urltransform rule doesn't break POST requests", async (assert) => {
-        const response = await fetch('https://httpbin.agrd.dev/royalmail', { method: 'POST', body: 'testdata' });
+        const response = await fetch('https://httpbin.agrd.dev/royalmail', {
+            method: 'POST',
+            body: 'testdata',
+            headers: { 'Accept': 'text/html' }
+        });
         assert.ok(adgCheck && response.ok, '$urltransform rule should not break POST requests');
     });
 
     agTest(3, '$urltransform rule can change the origin', async (assert) => {
         try {
-            const response = await fetch('https://example.org/status/200');
+            const response = await fetch('https://example.org/status/200', {
+                headers: { 'Accept': 'text/html' }
+            });
             assert.ok(adgCheck && response.ok, '$urltransform rule should change the origin');
         } catch (TypeError) {
             assert.true(false, '$urltransform rule should change the origin');
@@ -26,7 +34,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     agTest(4, "$urltransform rule doesn't change the origin for a POST request", async (assert) => {
         try {
-            const response = await fetch('https://example.org/post', { method: 'POST', body: 'testdata' });
+            const response = await fetch('https://example.org/post', {
+                method: 'POST',
+                body: 'testdata',
+                headers: { 'Accept': 'text/html' }
+            });
             assert.ok(adgCheck && !response.ok, '$urltransform rule should not change the origin for a POST request');
         } catch (TypeError) {
             // Succeed: TypeError is thrown because example.org don't send Access-Control-Allow-Origin with our origin
