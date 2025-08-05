64068ee1dde72e9b95ee282195cc5d6eb7c475aa
Aleksei Zhavoronkov <a.zhavoronkov@adguard.com>
2025-08-05 15:39:14 +0300

    Pull request #238: [AG-43083] Specify accept header to make these tests work
    Merge in ADGTEST/filters-tests from AG-43083 to master
    
    Squashed commit of the following:
    
    commit 30f80b97c1616b786a9698a032868ff74001703f
    Author: Alexey Zhavoronkov <a.zhavoronkov@adguard.com>
    Date:   Mon Aug 4 16:30:42 2025 +0300
    
        Add new changes to readme
    
    commit 31d8da071c66578ccc8207d2880629520f264f8e
    Author: Alexey Zhavoronkov <a.zhavoronkov@adguard.com>
    Date:   Fri Jun 27 11:17:25 2025 +0300
    
        Add new tests with content-type modifiers
    
    commit 0fc1f6b7b8569d4431db4eb3cbdb416f78e5a657
    Author: Alexey Zhavoronkov <a.zhavoronkov@adguard.com>
    Date:   Thu Jun 19 14:23:56 2025 +0300
    
        [AG-43083] Specify accept header to make these tests work
        
        Because $ urltransform rule no longer matches all request types, only document.
        
        See-also: AG-43083
        Signed-off-by: Alexey Zhavoronkov <a.zhavoronkov@adguard.com>

--- public/Filters/urltransform-rules/test-urltransform-rules.js
+++ public/Filters/urltransform-rules/test-urltransform-rules.js
@@ -6,18 +6,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const adgCheck = isSubscribed('subscribe-to-test-urltransform-rules-filter');
 
     agTest(1, '$urltransform rule', async (assert) => {
-        const response = await fetch('https://httpbin.agrd.dev/status/500');
+        const response = await fetch('https://httpbin.agrd.dev/status/500', {
+            headers: { Accept: 'text/html' },
+        });
         assert.ok(adgCheck && response.ok, '$urltransform rule should transform an invalid URL into a valid one');
     });
 
     agTest(2, "$urltransform rule doesn't break POST requests", async (assert) => {
-        const response = await fetch('https://httpbin.agrd.dev/royalmail', { method: 'POST', body: 'testdata' });
+        const response = await fetch('https://httpbin.agrd.dev/royalmail', {
+            method: 'POST',
+            body: 'testdata',
+            headers: { Accept: 'text/html' },
+        });
         assert.ok(adgCheck && response.ok, '$urltransform rule should not break POST requests');
     });
 
     agTest(3, '$urltransform rule can change the origin', async (assert) => {
         try {
-            const response = await fetch('https://example.org/status/200');
+            const response = await fetch('https://example.org/status/200', {
+                headers: { Accept: 'text/html' },
+            });
             assert.ok(adgCheck && response.ok, '$urltransform rule should change the origin');
         } catch (TypeError) {
             assert.true(false, '$urltransform rule should change the origin');
@@ -26,11 +34,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
     agTest(4, "$urltransform rule doesn't change the origin for a POST request", async (assert) => {
         try {
-            const response = await fetch('https://example.org/post', { method: 'POST', body: 'testdata' });
+            const response = await fetch('https://example.org/post', {
+                method: 'POST',
+                body: 'testdata',
+                headers: { Accept: 'text/html' },
+            });
             assert.ok(adgCheck && !response.ok, '$urltransform rule should not change the origin for a POST request');
         } catch (TypeError) {
             // Succeed: TypeError is thrown because example.org don't send Access-Control-Allow-Origin with our origin
             assert.true(true, '');
         }
+    });
+
+    agTest(5, '$urltransform rule should work with $script modifier', async (assert) => {
+        const response = await fetch('https://httpbin.agrd.dev/status/502', {
+            headers: { Accept: 'application/javascript' },
+        });
+        assert.ok(adgCheck && response.ok, 'script $urltransform should apply for JS resources');
+
+        const htmlResponse = await fetch('https://httpbin.agrd.dev/status/502', {
+            headers: { Accept: 'text/html' },
+        });
+        assert.ok(adgCheck && !htmlResponse.ok, 'script $urltransform should not apply for HTML');
+    });
+
+    agTest(6, '$urltransform rule should work with $image modifier', async (assert) => {
+        const response = await fetch('https://httpbin.agrd.dev/status/503', {
+            headers: { Accept: 'image/png' },
+        });
+        assert.ok(adgCheck && response.ok, 'image $urltransform should apply for image resources');
+
+        const htmlResponse = await fetch('https://httpbin.agrd.dev/status/503', {
+            headers: { Accept: 'text/html' },
+        });
+        assert.ok(adgCheck && !htmlResponse.ok, 'image $urltransform should not apply for HTML');
     });
 });
